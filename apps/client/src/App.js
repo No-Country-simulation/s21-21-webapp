55196ac51700c8db4e056758f65e8310a0651474
emablanco1800 <emablanco1800@gmail.com>
2025-02-14 20:33:27 -0300

    Carpeta client creada, instalacion de react y tailwind

---
+++ apps/client/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+        App de Cine
+      </button>
+    </div>
+  );
+}
+
+export default App;
