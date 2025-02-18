c8b0b03aa148d6e2450eeb639acd76ba6e3cdebd
zaidraed <zaidraed@gmail.com>
2025-02-18 16:42:01 -0300

    feat(images): add Cloudinary config and upload functionality with description

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { CloudinaryModule } from "./cloudinary/cloudinary.module";
+import { ImagesModule } from "./images/images.module";
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [CloudinaryModule, ImagesModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ apps/server/src/prisma/prisma.service.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from "@prisma/client";
+import { OnModuleInit } from "@nestjs/common";
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
