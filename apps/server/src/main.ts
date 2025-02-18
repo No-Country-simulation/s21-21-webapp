02ba6ea80fc62e0256dbdc606ac5112ac18b71d1
zaidraed <zaidraed@gmail.com>
2025-02-18 13:46:04 -0300

    feat: prisma module and auth module. include jwt.sttategy. (must npm install)

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,16 +1,20 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Configuración de Swagger
+  app.enableCors();
+
+  app.useGlobalPipes(new ValidationPipe());
+
   const config = new DocumentBuilder()
     .setTitle("API Cine")
     .setDescription("Documentation API Cine")
     .setVersion("1.0")
-    // .addBearerAuth() // Si usas autenticación con JWT
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
