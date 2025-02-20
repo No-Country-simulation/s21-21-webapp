9bcd214d6f6b9e05a752e32f2678698c51120e37
EstebanRataLiendo <esteban.cba92@gmail.com>
2025-02-20 14:47:43 -0300

    style: added new styles to login and register page

--- apps/client/src/Pages/Login/Login.jsx
+++ apps/client/src/Pages/Login/Login.jsx
@@ -1,48 +1,52 @@
 import React from 'react'
 
+
 export const Login = () => {
   return (
-    <div className="flex h-screen">
-      <div className="hidden lg:flex w-1/2 min-h-full bg-gray-300">
-        <img src="/Logo-Titulo.png" alt="Cine-Astas" className="object-contain"/>
-      </div>
+    <div className="flex">
+      <div className="hidden lg:flex w-1/2 min-h-155 bg-gray-300"></div>
       
       <div className="w-full lg:w-1/2 flex items-center justify-center p-6">
-        <div className="w-full max-w-106 bg-white p-3 rounded-lg">
-          <div className="lg:hidden mb-6 flex justify-center">
-            <img src="/Logo-Titulo.png" alt="Cine" className="object-contain w-3/4 h-auto max-w-lg" />
+        <div className="w-full max-w-sm bg-white p-3 rounded-lg">
+          <div className="mb-12 flex justify-center">
+            <img src="/Logo.png" alt="Cine" className="object-contain w-1/8 h-auto max-w-lg mr-5" />
+            <img src="/Titulo.png" alt="Cine" className="object-contain w-2/5 h-auto max-w-lg" />
           </div>
-          <h2 className="text-3xl font-bold text-justify-left mb-6">Iniciar sesión</h2>
-          <h3 className="text-base text-justify-left mb-6">Ingrese sus credenciales para iniciar sesión en su cuenta.</h3>
+          <h2 className="text-2xl font-bold text-justify-left mb-3">Iniciar sesión</h2>
+          <h3 className="text-sm text-justify-left mb-5">Ingrese sus credenciales para iniciar sesión en su cuenta.</h3>
           <form>
-            <div className="mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2">Correo Electrónico</label>
+            <div className="mb-3">
+              <label className="block text-gray-700 text-xs font-bold mb-1">Correo Electrónico</label>
               <input
                 type="email"
-                className="w-full max-w-80 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full max-w-80 p-0.5 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                 placeholder="correo@ejemplo.com"
                 required
               />
-              <label className="block text-gray-400 text-sm mb-2">Ingresa tu correo electrónico</label>
+              <label className="block text-gray-400 text-xs mb-1">Ingresa tu correo electrónico</label>
             </div>
             
-            <div className="mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2">Contraseña</label>
+            <div className="mb-3">
+              <label className="block text-gray-700 text-xs font-bold mb-1">Contraseña</label>
               <input
                 type="password"
-                className="w-full max-w-80 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full max-w-80 p-0.5 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                 placeholder="*******"
                 required
               />
-              <label className="block text-gray-400 text-sm mb-2">Ingresa una contraseña</label>
+              <label className="block text-gray-400 text-xs mb-1">Ingresa una contraseña</label>
             </div>
             
-            <button className="w-full max-w-80 bg-blue-950 text-white p-2 rounded-lg font-bold hover:bg-blue-600 transition">
+            <button className="w-full max-w-80 bg-btn-primary text-white p-1 rounded-sm font-bold hover:bg-btn-hover transition">
               Ingresar
             </button>
 
-            <div className="w-full max-w-80 mb-3 mt-3 text-center ">
+            <div className="w-full max-w-80 mb-1 mt-2 text-center ">
               <a href="#" className="text-blue-950 text-sm hover:underline">¿Olvidó tu contraseña?</a>
+            </div>
+            
+            <div className="w-full max-w-80 text-center ">
+              <a href="/Register" className="text-blue-950 text-sm hover:underline">¿No tienes una cuenta? <span className='font-bold'>Registrate</span></a>
             </div>
             
           </form>
