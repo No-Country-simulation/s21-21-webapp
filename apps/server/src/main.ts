9baff09c3656265e84638e0b7eb1c639ece8d61c
isakidev <gaspar.c.developer@gmail.com>
2025-02-21 20:34:03 -0300

    feat: implement logger with local url

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,10 +1,12 @@
+import { ValidationPipe, Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { ValidationPipe } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger()
 
   app.enableCors();
 
@@ -20,6 +22,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
 
-  await app.listen(3000);
+  const PORT = 3000 | 0
+
+  await app.listen(PORT);
+
+  logger.log(`Server listen in: http://localhost:${PORT}`)
 }
 bootstrap();
