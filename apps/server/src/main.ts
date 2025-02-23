e49546cf74a8cf63bd0792d3a8cc618b940e21d6
isakidev <gaspar.c.developer@gmail.com>
2025-02-23 20:08:37 -0300

    feat: add config to transfer buffers in webhook

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,7 +5,11 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true,
+    bodyParser: true
+  });
+
   const logger = new Logger()
 
   app.enableCors();
