e1739fcb3c83de5697bf70576246cec0bb7dd244
isakidev <gaspar.c.developer@gmail.com>
2025-03-03 03:05:55 -0300

    feat: add payment controller

--- apps/server/src/payments/payments.module.ts
+++ apps/server/src/payments/payments.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { PaymentsService } from './payments.service';
+import { PaymentsController } from './payments.controller';
 
 @Module({
+  controllers: [PaymentsController],
   providers: [PaymentsService],
   imports: [ConfigModule.forRoot()],
   exports: [
