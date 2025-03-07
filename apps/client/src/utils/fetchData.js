10db7fb0eedcecb747fb64fa9fe4597356a14e87
gabyp05 <gabyapd05@gmail.com>
2025-03-07 14:13:58 -0400

    refactor: improve fetchData error handling and response validation

--- apps/client/src/utils/fetchData.js
+++ apps/client/src/utils/fetchData.js
@@ -1,36 +1,28 @@
 export const fetchData = async (path, method, body, token) => {
-    try {
-      const URL = import.meta.env.VITE_BASE_URL;
-  
-      const headers = {}; 
-  
-      token && (headers["Authorization"] = `Bearer ${token}`);
-  
-      const options = { method, headers };
-  
-      if (body instanceof FormData) {
-        options.body = body;
-      } else if (body && method !== "GET") {
-        headers["Content-Type"] = "application/json"; 
-        options.body = JSON.stringify(body);
-      }
-  
-      const res = await fetch(`${URL}${path}`, options);
-  
-      if (!res.ok) {
-        const errorData = await res.json();
-        throw new Error(errorData.message || "Ha ocurrido un error");
-      }
-  
-      const contentType = res.headers.get("content-type");
-      if (contentType && contentType.includes("application/json")) {
-        return await res.json();
-      } else {
-        return await res.text(); 
-      }
-  
-    } catch (error) {
-      console.error(error);
-      throw error;
+  try {
+    const URL = import.meta.env.VITE_BASE_URL;
+    const headers = {};
+
+    token && (headers["Authorization"] = `Bearer ${token}`);
+
+    const options = { method, headers };
+
+    if (body instanceof FormData) {
+      options.body = body;
+    } else if (body && method !== "GET") {
+      headers["Content-Type"] = "application/json";
+      options.body = JSON.stringify(body);
     }
-  };
+
+    const res = await fetch(`${URL}${path}`, options);
+
+    if (!res) {
+      throw new Error("No se recibió respuesta del servidor.");
+    }
+
+    return res;
+  } catch (error) {
+    console.error("Error en fetchData:", error);
+    throw error;
+  }
+};
